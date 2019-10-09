bb42102ffd392d3d8fb375519247a07ebeda9c70
Surya Sanjiv <suryasanjiv@Suryas-MacBook-Pro.local>
2019-10-08 23:15:34 -0400

    updating oauth2.0 logic.. having issues with it still :(

--- app/credentials.py
+++ app/credentials.py
@@ -2,6 +2,7 @@ import spotipy
 import spotipy.oauth2 as oauth2
 import spotipy.util as util
 import os, sys
+from flask import Flask, request
 
 client_id = os.environ.get('CLIENT_ID')
 client_secret = os.environ.get('CLIENT_SECRET')
@@ -9,25 +10,10 @@ redirect_uri = os.environ.get('REDIRECT_URI')
 SCOPE = 'user-library-read'
 CACHE = '.spotipyoauthcache'
 
-sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret,redirect_uri)
 
-print(client_id)
-print(client_secret)
-print(redirect_uri)
-
-scope = 'user-library-read'
-
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print ("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
-
-token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-
-#credentials = oauth2.SpotifyClientCredentials(client_id, client_secret)
-# # token = credentials.get_access_token()
-# token = util.prompt_for_user_token(username, scope)
-# token = util.prompt_for_user_token(username, scope, client_id = 'your-app-redirect-url', client_secret=' your-app-redirect-url', redirect_uri = 'your-app-redirect-url')
-
-# spotify = spotipy.Spotify(auth=token)
+def promptUser(client_id, client_secret, redirect_uri):
+    sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret,redirect_uri)
+    url = sp_oauth.get_authorize_url()
+    code = sp_oauth.parse_response_code(url)
+    sp_oauth.get_access_token(code)
+    return url
--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
 # views.py
 
-from flask import render_template
+from flask import render_template, redirect
 from app import app
+from app import credentials
+from urllib import parse
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/auth', methods = ['GET'])
+def gen_playlist():
+    client_id = credentials.client_id
+    client_secret = credentials.client_secret
+    redirect_uri = credentials.redirect_uri
+    #credentials.getSpotifyInstance
+
+    # return credentials.promptUser(credentials.client_id, credentials.client_secret, credentials.redirect_uri)
+    # return redirect("https://accounts.spotify.com/en/authorize?client_id=%s&response_type=code&scope=%s&redirect_uri=%s" % (client_id, parse.quote_plus(credentials.SCOPE), parse.quote_plus(redirect_uri))))
+    # return credentials.promptUser(credentials.client_id, credentials.client_secret, credentials.redirect_uri)
+
+    return redirect(credentials.promptUser(credentials.client_id, credentials.client_secret, credentials.redirect_uri))
