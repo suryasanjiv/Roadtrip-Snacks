91c524815210831f3ae094f00cb2dd3292fdc9d4
Surya Sanjiv <suryasanjiv@Suryas-MacBook-Pro.local>
2019-10-10 00:49:48 -0400

    more fixes

--- app/credentials.py
+++ app/credentials.py
@@ -9,9 +9,8 @@ redirect_uri = os.environ.get('REDIRECT_URI')
 SCOPE = 'user-library-read'
 CACHE = '.spotipyoauthcache'
 
-
-def promptUser(id, secret, uri):
-    sp_oauth = oauth2.SpotifyOAuth(id, secret, uri)
+def promptUser():
+    sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri)
     url = sp_oauth.get_authorize_url()
     code = sp_oauth.parse_response_code(url)
     sp_oauth.get_access_token(code)
--- app/views.py
+++ app/views.py
@@ -11,13 +11,9 @@ def index():
 
 @app.route('/auth', methods = ['GET'])
 def gen_playlist():
-    client_id = credentials.client_id
-    client_secret = credentials.client_secret
-    redirect_uri = credentials.redirect_uri
-    #credentials.getSpotifyInstance
 
     # return credentials.promptUser(credentials.client_id, credentials.client_secret, credentials.redirect_uri)
     # return redirect("https://accounts.spotify.com/en/authorize?client_id=%s&response_type=code&scope=%s&redirect_uri=%s" % (client_id, parse.quote_plus(credentials.SCOPE), parse.quote_plus(redirect_uri))))
     # return credentials.promptUser(credentials.client_id, credentials.client_secret, credentials.redirect_uri)
 
-    return redirect(credentials.promptUser(credentials.client_id, credentials.client_secret, credentials.redirect_uri))
+    return redirect(credentials.promptUser())
